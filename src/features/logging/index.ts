77758a1a1ec06f3807acb475f66a2f1f5f89b990
mtsarac <mtsarac03@gmail.com>
2026-06-16 03:56:00 +0300

    chore: remove unused barrel files (7 index.ts)

--- src/features/logging/index.ts
+++
@@ -1,4 +0,0 @@
-export type { LoggingService } from './LoggingService'
-export { UmamiLogger } from './UmamiLogger'
-export { LoggingProvider, LoggingContext } from './LoggingProvider'
-export type { LoggingContextType } from './LoggingProvider'
