446db9dd08e21694ca014895e2f6ff1b2c2ab5d5
mtsarac <mtsarac@users.noreply.github.com>
2026-07-07 02:59:20 +0300

    feat: add scroll progress indicator

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from 'react'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
+import { ScrollProgress } from './ScrollProgress'
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollProgress />
       <Navbar />
       <main className="flex-1 pt-16">{children}</main>
       <Footer />
