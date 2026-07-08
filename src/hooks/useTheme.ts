bc2784555689fa2eda5b8d91f7b47142033b81cf
mtsarac <mtsarac@users.noreply.github.com>
2026-07-09 00:56:21 +0300

    chore: remove ThemeProvider, ThemeToggle, and useTheme

--- src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { ThemeContext } from '../features/theme/ThemeProvider'
-import type { ThemeContextType } from '../features/theme/ThemeProvider'
-
-export function useTheme(): ThemeContextType {
-  const ctx = useContext(ThemeContext)
-  if (!ctx) throw new Error('useTheme must be used within ThemeProvider')
-  return ctx
-}
